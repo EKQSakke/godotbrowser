ceca6bcc16fb9cb84f3fbd840f3e60a8cb9e7560
Sakari Ekqvist <sakari.ekqvist@magicad.com>
2023-01-14 18:52:31 +0200

    Unzipping to correct folder

--- filehandler.go
+++ filehandler.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"archive/zip"
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 func Download(url string, fileName string) error {
@@ -29,5 +31,43 @@ func Download(url string, fileName string) error {
 
 	// Write the body to file
 	_, err = io.Copy(out, resp.Body)
+	if (err != nil) {
+		return err
+	}
+
+	err = unzip(filepath, "C:/Program Files/Godot/" +  fileName)
 	return err
+}
+
+func unzip(src, dest string) error {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer rc.Close()
+
+		path := filepath.Join(dest, f.Name)
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, f.Mode())
+		} else {
+			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
