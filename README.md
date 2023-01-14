6690c8c61eb77ac72bba094265b66f8a7c3e817e
Sakari Ekqvist <sakari.ekqvist@magicad.com>
2023-01-14 18:34:53 +0200

    Separated Download to filehandler.go

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Godot browser
 
 ### TODO
-- Create bubbletea project with list of elements (use bubbles)
-- parse godot versions from web request
+- ✅Create bubbletea project with list of elements
+- ✅parse godot versions from web request
+- zip to folder: https://golang.cafe/blog/golang-unzip-file-example.html
+- scrollable list
--- main.go
+++ main.go
@@ -119,24 +119,3 @@ func (m model) View() string {
 	// Send the UI for rendering
 	return s
 }
-
-func Download(url string, fileName string) error {
-	filepath := "C:/temp/" + fileName
-	// Get the data
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	// Create the file
-	out, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
-	return err
-}
